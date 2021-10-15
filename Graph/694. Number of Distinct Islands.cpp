9dc5bd5c36a4750800ea4d1905a75e9d5b7c22f4
QUaSaR14 <madhurtrivedi14@gmail.com>
2021-10-15 21:48:42 +0530

    Solved: Number of Distinct Islands

---
+++ Graph/694. Number of Distinct Islands.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+static int X[4] = {-1, 0, 1, 0};
+static int Y[4] = {0, -1, 0, 1};
+static char D[4] = {'u', 'l', 'd', 'r'};
+class Solution {
+public: 
+    
+    void dfs(vector<vector<int>>& grid, string& str, int x, int y, const int& m, const int& n, char ch) {
+        
+        grid[x][y] = 0;
+        
+        str.push_back(ch);
+        
+        for(int k=0; k<4; k++) {
+            int i = X[k] + x;
+            int j = Y[k] + y;
+            
+            if(i>=0 && j>=0 && i<m && j<n && grid[i][j]) {
+                dfs(grid, str, i, j, m, n, D[k]);
+            }
+        }       
+    }
+    
+    int numDistinctIslands(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        unordered_set<string> st;
+        
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j]) {
+                    string s = "";
+                    dfs(grid, s, i, j, m, n, 'c'); 
+                    cout << s << endl;
+                    st.insert(s);   
+                }
+            }
+        }
+        
+        return st.size();
+    }
+};
