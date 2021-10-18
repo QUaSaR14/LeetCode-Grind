e9bb9b8130752f9b7cb4d05b6ba8fc48b5d8228f
QUaSaR14 <madhurtrivedi14@gmail.com>
2021-10-18 22:08:00 +0530

    Solved: Redundant Connection

---
+++ Graph/684. Redundant Connection.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class DSU {
+    vector<int> parent, size;
+public: 
+    
+    DSU(int n) {
+        for(int i=0; i<=n; i++) {
+            parent.push_back(i);
+            size.push_back(1);
+        }
+    }
+    
+    int findParent(int num) {
+        if(parent[num] == num) return num;
+        return parent[num] = findParent(parent[num]);
+    }
+    
+    bool unionBySize(int u, int v) {
+        int parU = findParent(u);
+        int parV = findParent(v);
+        
+        if(parU == parV) return false;
+        
+        if(size[parU] <= size[parV]) {
+            parent[parU] = parV;
+            size[parV] += size[parU];
+        }
+        else {
+            parent[parV] = parU;
+            size[parU] += size[parV];
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+        int n = edges.size();
+        
+        DSU dsu(n);
+        
+        for(auto& edge : edges) {
+            if(!dsu.unionBySize(edge[0], edge[1])) {
+                return edge;
+            }
+        }
+        return {};
+    }
+};
